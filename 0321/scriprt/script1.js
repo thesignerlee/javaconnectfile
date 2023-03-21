32911df3d168a5e849188a704e927ffbe04187d7
leejongjae <jongjealee@naver.com>
2023-03-21 10:31:54 +0900

    java_2nd

--- 0321/scriprt/script1.js
+++ 0321/scriprt/script1.js
@@ -12,8 +12,8 @@
 let a = 10 /* 변수 a의 개체값은 10이다 */
 let b = 'hello' /* b의 개체값은 hello다 */
 let c = a /* 변수 c의 개체값은 a와 같다. */
-console.log(a.length) /* 변수 a의 문자 길이를 콘솔창에 표시한다. */ /* undefined */
-console.log(a+b) /* 변수 a와 b를 더한 값을 콘솔창에 표시한다. */ /* 10hello */
-cobsole.log(typeof b) /* b의 함수를 콘솔창에 표시한다  */  /* error */
-window.alert(c) /* 변수 c를 윈도우 팝업창에 표시한다 */
-window.alert(a+c) /* 변수 a와 c를 더한 값을 윈도우 팝업창에 표시한다 */
+console.log(a.length) /* 변수 a의 문자 길이를 콘솔창에 표시한다. a는 문자열이 아니므로 출력할 수 없다고 뜸*/ /* undefined */
+console.log(a+b) /* 변수 a와 b를 더한 값을 콘솔창에 표시한다. 변수 a와 b의 데이터를 더하기, 연결하여 콘솔에 출력해라.*/ /* 10hello */
+console.log(typeof b) /* b의 함수를 콘솔창에 표시한다 데이터타입 연산자를 이용해서 b의 데이터 타입을 알려달라, b 문자 변수의 데이터를 콘솔에 출력해라 */  /* string */
+window.alert(c) /* 변수 c의 데이터를 윈도우 팝업창에 출력한다 */ // 10
+window.alert(a+c) /* 변수 a와 c를 더한 값을 윈도우 팝업창에 표시한다 새로고침 하면 뜸 */ //20 숫자끼리는 더하기, 숫자와 문자는 더하기
